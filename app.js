fd80c1a4e4a74d1d1b3adcc63027a4e54dfd2531
freezebear <ren.scarlet17@gmail.com>
2019-03-16 08:52:37 +0900

    Updated

--- app.js
+++ app.js
@@ -1,4 +1,98 @@
+/*
 var a1 = ' Hellow World~' ;
 var a2 = ' Hai Dunia~';
 
 console.log(a1 + a2);
+*/
+
+/*
+A ='mai nem';
+
+function logName(){
+    console.log(A);
+}
+
+logName();
+
+var A;
+*/
+
+/* varrriable
+var a
+var b {possible}
+
+let a
+let b {nope, just b}
+
+const a = 1 {same as let but can't give a new value}
+const a = 2 {error}
+*/
+
+/* arrray
+let A = ['satu', 'dua', '3'];
+a[1]; {yg muncul 'satu'}
+*/
+
+/*objeects 
+let dogo = {nama : 'doogoo' , asal : 'nepnep'};
+dogo.nama; {yg muncul doogoo}
+*/
+
+/* oprasi math
++ - / *
+=== {sama persis} !== {gk sama} < > <= >=
+10 % 3 = 1 {hasilnya dari 3 ke 10 = 3x tersisa 1}
+5 ** 2 = 25 {pangkat, sama dengan Math.pow(5, 2) }
+
+a++ /a-- {tambah 1}
+
+x = 3;
+x = x + 10;  {bisa juga menjadi x += 10} {berlaku untuk operasi yg lainnya juga}
+*/
+
+/*
+var myString = '123';
+var myNum = Number(myString);  {string ke int}
+typeof myNum;
+
+var myNum = 123;
+var myString = myNum.toString(); {int ke string}
+typeof myString;
+*/
+
+/*
+var tipe = 'freezebear';
+tipe.length;  {berapa banyak huruf yg ada}
+tipe[0] {huruf apa yg ada di [n] (starts from zero not one)}
+tipe[tipe.length-1] {huruf terakhir}
+tipe.indexOf('bea'); {berada di mana 'bea' di tipe}
+tipe.indexOf('bea') !== -1 {ada atau tidak}
+tipe.slice(0,6) {motong dari 0 ke 6}
+tipe.slice(6) {motong mulai dari 6}
+tipe.toUpperCase()/tipe.toLowerCase()
+tipe.replace('freeze','beku') {browserType = browerType.replace('freeze','beku');}
+
+*/
+
+
+/*
+var a1 = ['satu', 'dua', 3, 3, 5, [1, 'dua']];
+a1 [n]
+a1 [5][1] {array dalam array}
+
+var tenshi = ['hinata','hina','noa','kanon','koyori'];
+tenshi.split(','); 
+tenshi.join(',');  {bisa diganti sesuka hati}
+tenshi.toString(); {hanya koma}
+
+tenshi.push('myako'); {tambah dari belakang}
+tenshi.push('matsumoto', 'yuu');
+tenshi.unshift('') {sama seperti push tetapi dari depan}
+tenshi.pop(); {delete}
+tenshi.shift() {delete dari depan}
+*/
+
+
+/*
+
+*/
---
+++ prj2.1.js
@@ -0,0 +1,52 @@
+    
+        const a = document.querySelector('p');
+
+        a.addEventListener('click', updateName);
+
+        function updateName() {
+            let nama = prompt('Enter a new name');
+            a.textContent = 'Player 1: ' + nama;
+        }
+        
+        var butts = document.querySelectorAll('.ngeeng');
+
+                        for(let i = 0; i < butts.length ; i++) {
+                        butts[i].addEventListener('click', createParagraph);
+                }
+                
+                        function createParagraph() {
+                        let b = document.createElement('p');
+                        b.textContent = 'Honk Honk~';
+                        document.body.appendChild(b);
+                        }
+
+        var bb = document.querySelector('.buton1');
+        var teks = document.querySelector('.mulai');
+
+        bb.addEventListener('click', updatebb);
+
+        
+
+        function updatebb() {
+                if (bb.textContent === 'Start') {
+                        bb.textContent = 'Stop';
+                        teks.textContent = 'Starting Now!';
+
+                        
+                } 
+                else {
+                        bb.textContent = 'Start';
+                        teks.textContent = 'Stopped!';
+                        
+                       
+                
+                }
+
+        }
+
+ var but1 = document.querySelector('.helo');
+
+ but1.onclick = function() {
+ var nem = prompt('nama?');    //promt = ask form
+ alert('Hello ' + nem + ', nice to see you!');  //alert = note / notif
+}
